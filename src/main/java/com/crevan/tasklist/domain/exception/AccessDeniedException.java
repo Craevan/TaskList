86ffc485d7e022e5ba91497906751caa35faf76a
Crevan <a.siverskii@gmail.com>
2023-04-08 23:03:36 +0300

    [add] Exceptions

---
+++ src/main/java/com/crevan/tasklist/domain/exception/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package com.crevan.tasklist.domain.exception;
+
+public class AccessDeniedException extends RuntimeException {
+
+    public AccessDeniedException() {
+    }
+}
---
+++ src/main/java/com/crevan/tasklist/domain/exception/ResourceMappingException.java
@@ -0,0 +1,8 @@
+package com.crevan.tasklist.domain.exception;
+
+public class ResourceMappingException extends RuntimeException {
+
+    public ResourceMappingException(final String message) {
+        super(message);
+    }
+}
