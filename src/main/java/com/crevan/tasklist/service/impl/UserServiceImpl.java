39dd37477e3898c6ebcd9c61e1ba64c4bab23aa1
Crevan <a.siverskii@gmail.com>
2023-05-12 22:40:43 +0300

    [update] realisation of UserService

--- src/main/java/com/crevan/tasklist/service/impl/UserServiceImpl.java
+++ src/main/java/com/crevan/tasklist/service/impl/UserServiceImpl.java
@@ -1,39 +1,71 @@
 package com.crevan.tasklist.service.impl;
 
+import com.crevan.tasklist.domain.exception.ResourceNotFoundException;
+import com.crevan.tasklist.domain.user.Role;
 import com.crevan.tasklist.domain.user.User;
+import com.crevan.tasklist.repository.UserRepository;
 import com.crevan.tasklist.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
+    @Transactional(readOnly = true)
     public User getById(final Long id) {
-        return null;
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getByUserName(final String userName) {
-        return null;
+        return userRepository.findByUserName(userName)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
+    @Transactional
     public User create(final User user) {
-        return null;
+        if (userRepository.findByUserName(user.getName()).isPresent()) {
+            throw new IllegalStateException("User already exists");
+        }
+        if (!user.getPassword().equals(user.getPasswordConfirmation())) {
+            throw new IllegalStateException("Password and password confirmation do not match");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.create(user);
+        userRepository.insertUserRole(user.getId(), Role.ROLE_USER);
+        user.setRoles(Set.of(Role.ROLE_USER));
+        return user;
     }
 
     @Override
+    @Transactional
     public User update(final User user) {
-        return null;
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.update(user);
+        return user;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public boolean isTaskOwner(final Long userId, final Long taskId) {
-        return false;
+        return userRepository.isTaskOwner(userId, taskId);
     }
 
     @Override
+    @Transactional
     public void delete(final Long id) {
-
+        userRepository.delete(id);
     }
 }
