fcf107484c00f1ea6b4ea3e005ebee209f880fe7
Crevan <a.siverskii@gmail.com>
2023-05-12 22:43:03 +0300

    [update] realisation of TaskRepository

--- src/main/java/com/crevan/tasklist/repository/impl/TaskRepositoryImpl.java
+++ src/main/java/com/crevan/tasklist/repository/impl/TaskRepositoryImpl.java
@@ -1,41 +1,163 @@
 package com.crevan.tasklist.repository.impl;
 
+import com.crevan.tasklist.domain.exception.ResourceMappingException;
 import com.crevan.tasklist.domain.task.Task;
+import com.crevan.tasklist.repository.DataSourceConfig;
 import com.crevan.tasklist.repository.TaskRepository;
+import com.crevan.tasklist.repository.mapper.TaskRowMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class TaskRepositoryImpl implements TaskRepository {
+
+    private final DataSourceConfig dataSourceConfig;
+
+    private final String findById = """
+            SELECT t.id              AS task_id,
+                   t.title           AS task_title,
+                   t.description     AS task_description,
+                   t.expiration_date AS task_expiration_date,
+                   t.status          AS task_status
+            FROM tasks t
+            WHERE id = ?""";
+
+    private final String findAllByUserId = """
+            SELECT t.id              AS task_id,
+                   t.title           AS task_title,
+                   t.description     AS task_description,
+                   t.expiration_date AS task_expiration_date,
+                   t.status          AS task_status
+            FROM tasks t
+                     JOIN users_tasks ut on t.id = ut.task_id
+            WHERE ut.user_id = ?""";
+
+    private final String assign = """
+            INSERT INTO users_tasks (task_id, user_id)
+            VALUES (?, ?)""";
+
+    private final String update = """
+            UPDATE tasks
+            SET title = ?,
+                description = ?,
+                expiration_date = ?,
+                status = ?
+            WHERE id = ?""";
+
+    private final String create = """
+            INSERT INTO tasks (title, description, expiration_date, status)
+            VALUES (?, ?, ?, ?)""";
+
+    private final String delete = """
+            DELETE FROM tasks
+            WHERE id = ?""";
+
     @Override
     public Optional<Task> findById(final Long id) {
-        return Optional.empty();
+        try {
+            Connection connection = dataSourceConfig.getConnection();
+            PreparedStatement statement = connection.prepareStatement(findById);
+            statement.setLong(1, id);
+            try (ResultSet rs = statement.executeQuery()) {
+                return Optional.ofNullable(TaskRowMapper.mapRow(rs));
+            }
+        } catch (SQLException ex) {
+            throw new ResourceMappingException("Error while finding task by id");
+        }
     }
 
     @Override
     public List<Task> findAllByUserId(final Long userId) {
-        return null;
+        try {
+            Connection connection = dataSourceConfig.getConnection();
+            PreparedStatement statement = connection.prepareStatement(findAllByUserId);
+            statement.setLong(1, userId);
+            try (ResultSet rs = statement.executeQuery()) {
+                return TaskRowMapper.mapRows(rs);
+            }
+        } catch (SQLException ex) {
+            throw new ResourceMappingException("Error while finding all tasks by user Id");
+        }
     }
 
     @Override
     public void assignToUserById(final Long taskId, final Long userId) {
-
+        try {
+            Connection connection = dataSourceConfig.getConnection();
+            PreparedStatement statement = connection.prepareStatement(assign);
+            statement.setLong(1, taskId);
+            statement.setLong(2, userId);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new ResourceMappingException("Error while assigning to user");
+        }
     }
 
     @Override
     public void update(final Task task) {
-
+        try {
+            Connection connection = dataSourceConfig.getConnection();
+            PreparedStatement statement = connection.prepareStatement(update);
+            statement.setString(1, task.getTitle());
+            if (task.getDescription() == null) {
+                statement.setNull(2, Types.VARCHAR);
+            } else {
+                statement.setString(2, task.getDescription());
+            }
+            if (task.getExpirationDate() == null) {
+                statement.setNull(3, Types.TIMESTAMP);
+            } else {
+                statement.setTimestamp(3, Timestamp.valueOf(task.getExpirationDate()));
+            }
+            statement.setString(4, task.getStatus().name());
+            statement.setLong(5, task.getId());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new ResourceMappingException("Error while updating task");
+        }
     }
 
     @Override
     public void create(final Task task) {
-
+        try {
+            Connection connection = dataSourceConfig.getConnection();
+            PreparedStatement statement = connection.prepareStatement(create, PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setString(1, task.getTitle());
+            if (task.getDescription() == null) {
+                statement.setNull(2, Types.VARCHAR);
+            } else {
+                statement.setString(2, task.getDescription());
+            }
+            if (task.getExpirationDate() == null) {
+                statement.setNull(3, Types.TIMESTAMP);
+            } else {
+                statement.setTimestamp(3, Timestamp.valueOf(task.getExpirationDate()));
+            }
+            statement.setString(4, task.getStatus().name());
+            statement.executeUpdate();
+            try (ResultSet rs = statement.getGeneratedKeys()) {
+                rs.next();
+                task.setId(rs.getLong(1));
+            }
+        } catch (SQLException ex) {
+            throw new ResourceMappingException("Error while creating task");
+        }
     }
 
     @Override
     public void delete(final Long id) {
-
+        try {
+            Connection connection = dataSourceConfig.getConnection();
+            PreparedStatement statement = connection.prepareStatement(delete, PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new ResourceMappingException("Error while deleting task");
+        }
     }
 }
